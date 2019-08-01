4a0362e36b573286660896dde12d5845d54c567e
PacersCyc <452720855@qq.com>
2019-08-01 22:00:13 +0800

    add icons

---
+++ src/pages/Home/components/icons.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc: '杭州野生'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '杭州必游'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/75/eca3ce656c886502.png',
+          desc: '漂流'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        },
+        {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/75/eca3ce656c886502.png',
+          desc: '千岛湖'
+        },
+        {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+          desc: '浪浪浪水公园'
+        },
+        {
+          id: '0008',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/12375c435e1bc4a52074f84f40a685a0.png',
+          desc: '杭州极地'
+        },
+        {
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+          desc: '杭州宋城'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  @import '~styles/mixins.styl';
+  .icons >>> .swiper-container
+    height: 0;
+    padding-bottom: 50%;
+  .icon
+    overflow hidden;
+    width: 25%;
+    height: 0;
+    float: left;
+    padding-bottom: 25%;
+    position relative;
+    .icon-img
+      box-sizing: border-box;
+      padding: .1rem;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: .44rem;
+      .icon-img-content
+        display: block;
+        margin: 0 auto;
+        height: 100%;
+    .icon-desc
+      position: absolute;
+      bottom: 0;
+      left: 0
+      right: 0;
+      line-height: .44rem;
+      height: .44rem;
+      color: $darkTextColor;
+      text-align: center;
+      ellipsis()
+</style>
